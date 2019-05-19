aa973a5a1fe576fcfb499c4bd200ba8c71c339a7
AnilSenV2 <fb25@live.nl>
2019-05-19 10:35:39 +0200

    Add files via upload

---
+++ GARAGE + LOGIN + front design+ opdracht 14/gar-search-auto1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta name="author" content="Anil Sen">
+    <meta charset="UTF-8">
+    <title>Garage search auto1</title>
+</head>
+<body>
+<h1>Garage zoek op autokenteken 1</h1>
+<p>
+    Dit formulier zoekt een autokenteken op uit
+    de tabel auto van database garage.
+</p>
+<form action="gar-search-auto2.php" method="post">
+    Welke Autokenteken zoekt u?
+    <input type="text" name="autokentekenvak"> <br />
+    <input type="submit">
+</form>
+
+</body>
+</html>
